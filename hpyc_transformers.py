4c82c18464cc808f60371d5fe31b609fe8194841
Ian Morgan <ian.j.morgan@gmail.com>
2023-03-02 19:28:48 +0000

    added ContentPanelTransformer

--- hpyc_transformers.py
+++ hpyc_transformers.py
@@ -3,16 +3,30 @@ from transformer import Transform
 
 class TopPanelTransformer(Transform):
     def transform(self, nodes: list) -> str:
-        result = '\n'
-        result += '<div class="columns col-gapless hpyc-section\">\n'
+        result = '<div class="columns col-gapless hpyc-section\">\n'
         result += '\t<div class=\"column col-12\">\n'
 
         for node in nodes:
             if node.tag == 'h1':
                 result += '\t\t' + '<h1>' + str(node.text) + '</h1>\n'
             elif node.tag == 'p':
-                result += '\t\t' + '<p>' + str(node.text)+ '</p>\n'
+                result += '\t\t' + '<p>' + str(node.text) + '</p>\n'
 
         result += "\t" + '</div>\n'
         result += '</div>\n'
-        return result.replace("\t","    ")
+        return result.replace("\t", "    ")  # 4 spaces per tab for consistent formatting
+
+
+class ContentPanelTransformer(Transform):
+    def transform(self, nodes: list) -> str:
+        result = '\n'
+        result += '<div class="columns col-gapless hpyc-section\">\n'
+        result += '\t<div class=\"column col-9\">\n'
+
+        for node in nodes:
+            if node.tag == 'p':
+                result += '\t\t' + '<p>' + str(node.text) + '</p>\n'
+
+        result += "\t" + '</div>\n'
+        result += '</div>\n'
+        return result.replace("\t", "    ")  # 4 spaces per tab for consistent formatting
--- test_hpyc_transformers.py
+++ test_hpyc_transformers.py
@@ -1,5 +1,6 @@
 from textwrap import dedent
 from hpyc_transformers import TopPanelTransformer
+from hpyc_transformers import ContentPanelTransformer
 from transformer import Transformer
 
 
@@ -20,8 +21,27 @@ def test_top_panel_transform():
                 <p>Paragraph 2</p>
             </div>
         </div>
-    """)
+    """).strip()
 
     transformer = Transformer(TopPanelTransformer('hpyc-top-panel'))
     transformed = transformer.transform(raw)
-    assert transformed == expected
+    assert transformed.strip() == expected
+
+
+def test_content_panel_transform():
+    raw = dedent("""
+        <hpyc-content-panel>
+            <p>a content panel with an image</p>
+        </hpyc-content-panel>""")
+
+    expected = dedent("""
+        <div class="columns col-gapless hpyc-section">
+            <div class="column col-9">
+                <p>a content panel with an image</p>
+            </div>
+        </div>
+      """).strip()
+
+    transformer = Transformer(ContentPanelTransformer('hpyc-content-panel'))
+    transformed = transformer.transform(raw)
+    assert transformed.strip() == expected
